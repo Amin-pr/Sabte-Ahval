ca95644b5c1a7c2820dd433e1b300d065b4ff00b
Amin <127015189+Amin-pr@users.noreply.github.com>
2024-07-08 12:03:52 +0330

    "Updated App.jsx and DataGrid.jsx: modified column width, uncommented rows in App.jsx, and passed rows as a prop to DataTable in DataGrid.jsx"

--- src/ui/components/table/DataGrid.jsx
+++ src/ui/components/table/DataGrid.jsx
@@ -47,18 +47,18 @@ import styles from "../table/DataGrid.module.scss";
 // ];
 
 // eslint-disable-next-line react/prop-types
-export default function DataTable({ columns }) {
-	const rows = [
-		{ id: 1, status: "Snow", location: "Jon", rank: 14 },
-		{ id: 2, lastName: "Lannister", firstName: "Cersei", age: 31 },
-		{ id: 3, lastName: "Lannister", firstName: "Jaime", age: 31 },
-		{ id: 4, lastName: "Stark", firstName: "Arya", age: 11 },
-		{ id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-		{ id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-		{ id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-		{ id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-		{ id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-	];
+export default function DataTable({ columns,rows }) {
+	// const rows = [
+	// 	{ id: 1, status: "Snow", location: "Jon", rank: 14 },
+	// 	{ id: 2, lastName: "Lannister", firstName: "Cersei", age: 31 },
+	// 	{ id: 3, lastName: "Lannister", firstName: "Jaime", age: 31 },
+	// 	{ id: 4, lastName: "Stark", firstName: "Arya", age: 11 },
+	// 	{ id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+	// 	{ id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+	// 	{ id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+	// 	{ id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+	// 	{ id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+	// ];
 
 	const downloadCols = columns?.map((column) => column.headerName).reverse();
 	return (
